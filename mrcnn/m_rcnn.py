73dd46a482a24558957a9562be82ba4a19e8110c
Harshini V Bhat <82927905+HarshiniBhat@users.noreply.github.com>
2022-07-20 15:34:37 +0530

    Update m_rcnn.py

--- mrcnn/m_rcnn.py
+++ mrcnn/m_rcnn.py
@@ -33,7 +33,7 @@ warnings.filterwarnings('ignore')
 MODEL_DIR = os.path.join("/content/drive/MyDrive/Mask_rcnn", "logs")
 
 # Local path to trained weights file
-COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+COCO_MODEL_PATH = os.path.join(MODEL_DIR, "mask_rcnn_coco.h5")
 # Download COCO trained weights from Releases if needed
 if not os.path.exists(COCO_MODEL_PATH):
     utils.download_trained_weights(COCO_MODEL_PATH)
@@ -264,10 +264,10 @@ def load_image_dataset(annotation_path, dataset_path, dataset_type):
 # Passing layers="heads" freezes all layers except the head
 # layers. You can also pass a regular expression to select
 # which layers to train by name pattern.
-def train_head(model, dataset_train, dataset_val, config):
+def train_head(model, dataset_train, dataset_val, config,epochs):
     model.train(dataset_train, dataset_val,
             learning_rate=config.LEARNING_RATE,
-            epochs=20,
+            epochs=epochs,
             layers='heads')
 
 
